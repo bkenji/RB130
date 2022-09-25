de3c5413141c07c290faa40e7f5c15ec41e6cf6e
Brenno Kenji <brennokenji@gmail.com>
2022-09-25 10:37:32 -0700

    accounting for edge-case in which acc explicit initial value  == arr[0]

--- lesson_1/reduce_method.rb
+++ lesson_1/reduce_method.rb
@@ -6,55 +6,35 @@
 
 # It is also possible to initialize the accumulator to a default value.
 
-# Implementation with a while loop:
+def reduce(arr, acc = nil)
+  acc.nil? ? (acc = arr[0]) && (counter = 1) : counter = 0
 
-# def reduce(arr, acc = 0)
-#   counter = 0
-
-#   while counter < arr.size
-#     acc = yield(acc, arr[counter])
-#     counter += 1
-#   end
-
-#   acc
-# end
-
-# Implementation with `Array#each`:
-
-def reduce(arr, acc = arr[0])
-  counter = 0
-
-  if acc == arr[0]
-    arr[1..-1].each do |element|
-      acc = yield(acc, element)
-      counter += 1
-    end
-  else
-    arr.each do |element|
-      acc = yield(acc, element)
-      counter += 1
-    end
+  while counter < arr.size
+    acc = yield(acc, arr[counter])
+    counter += 1
   end
 
   acc
 end
 
 array = [1, 2, 3, 4, 5]
+p array.reduce(1) {|acc, num|  acc + num}
+p reduce(array, 1) {|acc, num| acc + num}
 
 p reduce(array) { |acc, num| acc + num }                    # => 15
 p reduce(array, 10) { |acc, num| acc + num }                # => 25
 p array.reduce(10) { |acc, num| acc + num }                 # => 25
-# reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
-puts
+# # reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+# puts
 p array.reduce { |acc, num| acc * num }                     # => 120
 p array.reduce(2) { |acc, num| acc * num }                  # => 240
 p array.reduce { |acc, num| acc += num }                    # =>  15
-puts
+# puts
 p reduce(array) { |acc, num| acc * num }                    # => 120
 p reduce(array, 2) { |acc, num| acc * num }                 # => 240
 p reduce(array) { |acc, num| acc += num }                   # => 15
-# # p reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
-puts
+# # # p reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+# puts
 p reduce(["a", "b", "c"]) { |acc, value| acc += value }     # => 'abc'
 p reduce([[1, 2], ["a", "b"]]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']
 puts
