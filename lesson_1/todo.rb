088227cc5e100fbe2854336ea7057f57d0106b8b
Brenno Kenji <brennokenji@gmail.com>
2022-09-24 19:14:40 -0700

    TodoList + each method with block

--- lesson_1/todo.rb
+++ lesson_1/todo.rb
@@ -136,71 +136,75 @@ list.add(todo3)                 # adds todo3 to end of list, returns list
 # <<
 # same behavior as add
 
+list.each do |todo|
+  puts todo
+end
+
 # ---- Interrogating the list -----
 
-# # size
-p list.size                       # returns 3
+# # # size
+# p list.size                       # returns 3
 
-# # first
-p list.first                      # returns todo1, which is the first item in the list
+# # # first
+# p list.first                      # returns todo1, which is the first item in the list
 
-# # last
-p list.last                       # returns todo3, which is the last item in the list
+# # # last
+# p list.last                       # returns todo3, which is the last item in the list
 
-# #to_a
-p list.to_a                      # returns an array of all items in the list
+# # #to_a
+# p list.to_a                      # returns an array of all items in the list
 
-# #done?
-p list.done?                     # returns true if all todos in the list are done, otherwise false
+# # #done?
+# p list.done?                     # returns true if all todos in the list are done, otherwise false
 
-# # ---- Retrieving an item in the list ----
+# # # ---- Retrieving an item in the list ----
 
-# # item_at
-# list.item_at                    # raises ArgumentError
-p list.item_at(1)                 # returns 2nd item in list (zero based index)
-# list.item_at(100)               # raises IndexError
+# # # item_at
+# # list.item_at                    # raises ArgumentError
+# p list.item_at(1)                 # returns 2nd item in list (zero based index)
+# # list.item_at(100)               # raises IndexError
 
-# # ---- Marking items in the list -----
+# # # ---- Marking items in the list -----
 
-# # mark_done_at
-# list.mark_done_at               # raises ArgumentError
-p list.mark_done_at(1)            # marks the 2nd item as done
-# list.mark_done_at(100)          # raises IndexError
+# # # mark_done_at
+# # list.mark_done_at               # raises ArgumentError
+# p list.mark_done_at(1)            # marks the 2nd item as done
+# # list.mark_done_at(100)          # raises IndexError
 
-# # mark_undone_at
-# list.mark_undone_at             # raises ArgumentError
-p list.mark_undone_at(1)          # marks the 2nd item as not done,
-# list.mark_undone_at(100)        # raises IndexError
+# # # mark_undone_at
+# # list.mark_undone_at             # raises ArgumentError
+# p list.mark_undone_at(1)          # marks the 2nd item as not done,
+# # list.mark_undone_at(100)        # raises IndexError
 
-# # done!
-# p list.done!                      # marks all items as done
+# # # done!
+# # p list.done!                      # marks all items as done
 
-# # ---- Deleting from the list -----
+# # # ---- Deleting from the list -----
 
-# # shift
-# p list.shift                      # removes and returns the first item in list
+# # # shift
+# # p list.shift                      # removes and returns the first item in list
 
-# # pop
-# p list.pop                        # removes and returns the last item in list
+# # # pop
+# # p list.pop                        # removes and returns the last item in list
 
-# # remove_at
-# list.remove_at                  # raises ArgumentError
-# list.remove_at(1)               # removes and returns the 2nd item
-# list.remove_at(100)             # raises IndexError
+# # # remove_at
+# # list.remove_at                  # raises ArgumentError
+# # list.remove_at(1)               # removes and returns the 2nd item
+# # list.remove_at(100)             # raises IndexError
 
-# # ---- Outputting the list -----
+# # # ---- Outputting the list -----
 
-# # to_s
-puts list               # returns string representation of the list
+# # # to_s
+# puts list               # returns string representation of the list
 
-# # ---- Today's Todos ----
-# # [ ] Buy milk
-# # [ ] Clean room
-# # [ ] Go to gym
+# # # ---- Today's Todos ----
+# # # [ ] Buy milk
+# # # [ ] Clean room
+# # # [ ] Go to gym
 
-# # or, if any todos are done
+# # # or, if any todos are done
 
-# # ---- Today's Todos ----
-# # [ ] Buy milk
-# # [X] Clean room
-# # [ ] Go to gym
+# # # ---- Today's Todos ----
+# # # [ ] Buy milk
+# # # [X] Clean room
+# # # [ ] Go to gym
